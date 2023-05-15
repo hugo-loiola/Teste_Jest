54fa5067d2c9a6e4ecaa5f1e30a35f2f8bc2ead5
Hugo Loiola <hugoloiola.1@gmail.com>
2023-05-15 20:19:30 -0300

    primeiro teste

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+function somar(a, b) {
+  return a + b;
+}
+function multiplicar(a, b) {
+  return a * b;
+}
+function subtrair(a, b) {
+  return a - b;
+}
+function dividir(a, b) {
+  return a / b;
+}
+exports.somar = somar;
+exports.multiplicar = multiplicar;
+exports.subtrair = subtrair;
+exports.dividir = dividir;
---
+++ calculadora.test.js
@@ -0,0 +1,16 @@
+const calculadora = require("./calculadora");
+
+describe("Testando a calculadora", () => {
+  test("1+2", () => {
+    expect(calculadora.somar(1, 2)).toBe(3);
+  });
+  test("1 * 0 = 0", () => {
+    expect(calculadora.multiplicar(1, 0)).toEqual(0);
+  });
+  test("2 - 5 <> 3", () => {
+    expect(calculadora.subtrair(2, 5)).not.toBe(3);
+  });
+  test("5 / 3 ≃ 1.7", () => {
+    expect(calculadora.dividir(5, 3)).toBeCloseTo(1.7, 1);
+  });
+});
